3c4d72b2122c89a05ed64fbd1c05e53b0b94e34f
Aleksandar <sleasarbajic@gmail.com>
2023-11-10 22:45:19 +0100

    The World Wise App 🌐

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,12 @@ import City from "./components/City";
 import Form from "./components/Form";
 import SpinnerFullPage from "./components/SpinnerFullPage";
 
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import Homepage from "./pages/Homepage";
-// import Login from "./pages/Login";
-// import AppLayout from "./pages/AppLayout";
-// import PageNotFound from "./pages/PageNotFound";
-
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Product = lazy(() => import("./pages/Product"));
 const Pricing = lazy(() => import("./pages/Pricing"));
 const Login = lazy(() => import("./pages/Login"));
 const AppLayout = lazy(() => import("./pages/AppLayout"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
-
-// dist/assets/index-59fcab9b.css   30.56 kB │ gzip:   5.14 kB
-// dist/assets/index-f7c12d89.js   572.44 kB │ gzip: 151.29 kB
 
 function App() {
   return (
